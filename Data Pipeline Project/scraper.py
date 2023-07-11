2ca70304a8be1782de0a383854b6cc769bdc8464
Corbie <nicholaicorbie1@gmail.com>
2023-07-11 09:03:03 +0100

    adding correct repo files in

--- Data Pipeline Project/scraper.py
+++ Data Pipeline Project/scraper.py
@@ -2,7 +2,7 @@ import errno
 from typing import ChainMap
 import requests
 from selenium.webdriver import Chrome
-from webdriver_manager.chrome import ChromeDriverManager
+from webdrivermanager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -24,6 +24,7 @@ import os
 import pymysql
 from sqlalchemy import create_engine
 
+
 url = 'https://www.op.gg/'
 
 class Scraper():
@@ -31,7 +32,7 @@ class Scraper():
     def __init__(self): 
         options = Options()
         #options.add_argument('--headless')
-        self.driver = Chrome(ChromeDriverManager().install(), options=options)
+        self.driver = Chrome(r"C:\Users\nickc\OneDrive\Desktop\Code\AiCore\Data Pipeline Project\chromedriver")
         self.driver.maximize_window()
         self.display_dict = {
                         'Champion_Name' : [],
@@ -56,7 +57,7 @@ class Scraper():
 
     def bypass_cookies(self):
         try:
-            button_continer = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@class="qc-cmp2-summary-buttons"]')))
+            button_continer = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="qc-cmp2-summary-buttons"]')))
             buttons = button_continer.find_elements(By.XPATH, './button')
             accept_cookies_button = buttons[1]
             accept_cookies_button.click()
@@ -176,17 +177,12 @@ class Scraper():
                     champion_counters = champion_counters_container1.find_element(By.TAG_NAME, 'img')
                     champion_counter1 = champion_counters.get_attribute("alt")
                     champion_counter_list.append(champion_counter1)
-                self.display_dict['Champion_Counters'].append(champion_counter_list)
-        dict_counter = 0
-        for l in self.display_dict['Champion_Name']:
-            try:
-                self.get_champion_page(l)
-                self.get_images(self.display_dict['Champion_Page_Link'][dict_counter], l)
-                print("image link added")
-                dict_counter += 1
-            except Exception as e:
+                self.display_dict['Champion_Counters'].append(champion_counter_list) 
+        try:
+            names_links = self.get_unique_champion_name_and_link()      
+            self.put_champion_links_in_dictionary(names_links)
+        except Exception as e:
                 print(e)
-                self.driver.refresh()
         print(self.display_dict['Lane'])
         print(len((self.display_dict['Lane'])))
         print(self.display_dict['Champion_Rank'])
@@ -211,6 +207,58 @@ class Scraper():
         print(len((self.display_dict['Patch'])))
         print(number)
         pass
+    
+    def get_unique_champion_name_and_link(self):
+        champion_info = {}
+        counter = 1
+        for champion in self.display_dict['Champion_Name']:
+            champ_data = []
+            if champion not in champion_info:
+                champion_name = champion.lower()
+                if champion_name == 'nunu & willump':
+                    champion_name = 'nunu'
+                    champ_data.append(champion_name)
+                    print(champion_name)
+                elif champion_name == 'renata glasc':
+                    champion_name = 'renata'
+                    champ_data.append(champion_name)
+                    print(champion_name) 
+                else:
+                    champion_name = list(champion_name)
+                    print(champion_name)
+                    champion_name_list_counter = 0
+                    switch_check = True 
+                    for i in champion_name:
+                        if i.isalpha() is False and switch_check is True:
+                            champion_name[champion_name_list_counter] = '-'
+                            switch_check = False
+                        elif i.isalpha() is False and switch_check is False:
+                            champion_name.pop(champion_name_list_counter)
+                        champion_name_list_counter += 1
+                    champion_name = ''.join(champion_name)
+                    champ_data.append(champion_name)
+                    print(champion_name)
+                self.driver.get(f'https://www.leagueoflegends.com/en-gb/champions/{champion_name}/')
+                time.sleep(1)
+                self.accept_lol_page_cookies()
+                time.sleep(1)
+                image_contianer = self.driver.find_element(By.XPATH, '//div[@class="style__BackgroundImage-sc-8gkpub-2 WSJaH"]')
+                image_tag = image_contianer.find_element(By.XPATH, './img')
+                image_link = image_tag.get_attribute('src')
+                champ_data.append(image_link)
+                champion_info[champion] = champ_data
+                counter += 1
+            print(counter)
+        print(champion_info)
+        return champion_info
+
+
+    def put_champion_links_in_dictionary(self, info_dict):
+        for i in self.display_dict['Champion_Name']:
+            self.display_dict['Champion_Page_Link'].append(info_dict[i][1])
+        print(self.display_dict['Champion_Page_Link'])
+        pass
+
 
     def create_uuid(self):
         for i in self.display_dict['Champion_Name']:
@@ -225,40 +273,9 @@ class Scraper():
         table = pd.DataFrame(self.display_dict)
         print(table)
         return table
-
-    def get_champion_page(self, champion):
-        champion_name = champion.lower()
-        if champion_name == 'nunu & willump':
-            champion_name = 'nunu'
-            print(champion_name)
-        elif champion_name == 'renata glasc':
-            champion_name = 'renata'
-            print(champion_name) 
-        else:
-            champion_name = list(champion_name)
-            print(champion_name)
-            champion_name_list_counter = 0
-            switch_check = True 
-            for i in champion_name:
-                if i.isalpha() is False and switch_check is True:
-                    champion_name[champion_name_list_counter] = '-'
-                    switch_check = False
-                elif i.isalpha() is False and switch_check is False:
-                    champion_name.pop(champion_name_list_counter)
-                champion_name_list_counter += 1
-            champion_name = ''.join(champion_name)
-            print(champion_name)            
-        self.driver.get(f'https://www.leagueoflegends.com/en-gb/champions/{champion_name}/')
-        time.sleep(1)
-        self.accept_lol_page_cookies()
-        time.sleep(1)
-        image_contianer = self.driver.find_element(By.XPATH, '//div[@class="style__BackgroundImage-sc-8gkpub-2 WSJaH"]')
-        image_tag = image_contianer.find_element(By.XPATH, './img')
-        image_link = image_tag.get_attribute('src')
-        self.display_dict['Champion_Page_Link'].append(image_link)
-        pass
     
-    def create_folder(self, folder_name=r"/Champion Info", parent_directory=r"/Users/nick/Desktop/Code/AiCore/AiCore/Data Pipeline Project"):
+    
+    def create_folder(self, folder_name=r"\Champion Info", parent_directory=r"C:\Users\nickc\OneDrive\Desktop\Code\AiCore\Data Pipeline Project"):
         try:
             path = parent_directory + folder_name 
             if not os.path.isdir(path):
@@ -270,7 +287,7 @@ class Scraper():
         pass
 
     def get_images(self, url, champion_name):
-        urllib.request.urlretrieve(url, f'/Users/nick/Desktop/Code/AiCore/AiCore/Data Pipeline Project/Champion Info/{champion_name}.jpg')
+        urllib.request.urlretrieve(url, rf"C:\Users\nickc\OneDrive\Desktop\Code\AiCore\Data Pipeline Project\Champion Info\{champion_name}.jpg")
         pass
 
     def accept_lol_page_cookies(self):
@@ -278,8 +295,8 @@ class Scraper():
             time.sleep(2)
             accept_button = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, '//button[@class="osano-cm-accept-all osano-cm-buttons__button osano-cm-button osano-cm-button--type_accept"]')))
             accept_button.click()
-        except Exception:
-            print("No cookies found.")
+        except Exception as e:
+            print(e)
         pass
 
     def accept_lol_pic_page_cookies(self):
@@ -291,43 +308,40 @@ class Scraper():
         pass
 
     def create_json_file(self):
-        with open(r'/Users/nick/Desktop/Code/AiCore/AiCore/Data Pipeline Project/Champion Info/champion_info.json', 'w') as raw_data_file:
+        with open(r'C:\Users\nickc\OneDrive\Desktop\Code\AiCore\Data Pipeline Project\Champion Info\champion_info.json', 'w') as raw_data_file:
             json.dump(json.dumps(self.display_dict, indent = 4), raw_data_file)
 
     def create_s3_bucket(self, bucket_name='lolchampiondata', Location='eu-west-2', 
-    access_key_id='AKIA4BMRGIVBHNGBX26U', secret_access_key='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'):
+        access_key_id='AKIA4BMRGIVBJ5AVGRII', secret_access_key='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'):
         session = boto3.Session(aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
         s3 = session.resource('s3')
         try:
             s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': Location})
-            bucket = s3.Bucket(bucket_name)
-            bucket.Acl().put(ACL='private')
             print(f'The s3_bucket {bucket_name} was successfully created')
         except Exception as e:
             print(e)
         pass
 
     def upload_data_to_s3_bucket(self, file_name='Champion Info/', bucket_name='lolchampiondata', 
-    access_key_id='AKIA4BMRGIVBHNGBX26U', secret_access_key='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'):
+        access_key_id='AKIA4BMRGIVBJ5AVGRII', secret_access_key='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'):
         session = boto3.Session(aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
         s3 = session.resource('s3')
         bucket = s3.Bucket(bucket_name)
     
         list_of_files = os.listdir(file_name)
         for file in list_of_files:
-            file_path = rf'/Users/nick/Desktop/Code/AiCore/AiCore/Data Pipeline Project/Champion Info/{file}'
+            file_path = rf'C:\Users\nickc\OneDrive\Desktop\Code\AiCore\Data Pipeline Project\Champion Info\{file}'
             file_name = os.path.basename(file_path)
             try:
                 bucket.upload_file(file_path, file_name)
                 print('File was uploaded to s3 bucket successfully.')
             except Exception as e:
                 print(e)
-                print('The file failed to upload to the s3 bucket.')
         pass
     
-    def upload_dataframe_to_rds(self, host = 'lolchampiondatabase.cudnxxlhkqef.eu-west-2.rds.amazonaws.com',
+    def upload_dataframe_to_rds1(self, host = 'lolchampiondatabase.cudnxxlhkqef.eu-west-2.rds.amazonaws.com',
         port = int(3306),
-        user = 'admincorbie',
+        user = 'corbie',
         password = 'leagueoflegends',
         database = 'lolchampiondatabase'):
         
@@ -338,6 +352,10 @@ class Scraper():
             print('Creation of dataframe was unsuccessful.')
             print(e)
         
+        conn = pymysql.connect(host=host, user=user, passwd=password, connect_timeout=10)
+        with conn.cursor() as cur:
+            cur.execute('create database bsb-round;')
+            
         try:
             championdatadb = create_engine('mysql+pymysql://' + user + ':' + password + '@' + host + ':' + str(port) + '/' + database , echo=False)
             print('Create_engine successful.')
@@ -347,14 +365,15 @@ class Scraper():
             print(e)
         pass
 
-def upload_dataframe_to_rds(host = 'lolchampiondatabase.cudnxxlhkqef.eu-west-2.rds.amazonaws.com',
-        port = int(3306),
-        user = 'admincorbie',
+
+    def upload_dataframe_to_rds2(host = 'lolchampiondatabase.cudnxxlhkqef.eu-west-2.rds.amazonaws.com',
+        port = str(3306),
+        user = 'corbie',
         password = 'leagueoflegends',
         rds_database = 'lolchampiondatabase',
         sql_database = 'tabl'):
         
-        alcheng = create_engine('mysql+pymysql://' + user + ':' + password + '@' + host + ':' + str(port) + '/' + sql_database , echo=False)
+        alcheng = create_engine('mysql+pymysql://' + user + ':' + password + '@' + host + ':' + port + '/' + sql_database , echo=False)
         
         connection = pymysql.connect(host=host,
                              user=user,
