868995e78640aa86b213ac009afe4269617dbaaa
NICK <nicholaicorbie@yahoo.com>
2025-04-29 15:28:40 +0100

    changing the name of the EDA file from ML_Project to ExploratoryDataAnalysis

---
+++ ExploratoryDataAnalysis/data_info.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+class DataFrameInfo:
+
+    def __init__(self) -> None:
+        pass
+
+    def describe_df_columns(self, dataframe):
+        print("The information for this dataframe is as follows:\n")
+        dataframe.info()
+        pass
+
+    def get_statistical_df_info(self, dataframe):
+        pd.options.display.float_format = '{:.2f}'.format
+        print(f"The median values for this dataframe is as follows:\n{dataframe.median()}")    
+        print(f"The mean values for this dataframe is as follows:\n{dataframe.mean()}")
+        print(f"The standard deviation values for this dataframe is as follows:\n{dataframe.std()}")
+        pass
+
+    def show_column_distinct_values(self, dataframe):
+        for column in dataframe:
+            print(dataframe[column].value_counts())
+        pass
+
+    def print_dataframe_shape(self, dataframe):
+        print(f"The dataframe has the following shape:\n{dataframe.shape}")
+        pass
+
+    def column_null_count(self, dataframe):
+        pd.options.mode.use_inf_as_na = True
+        df_showing_nulls = dataframe.isna()
+        print("The count of nulls for each column is as follows:\n")
+        for column in df_showing_nulls:
+            print(df_showing_nulls[column].value_counts())
+        print("\n")
+        print("The proportion of nulls and non_nulls for each column are as follows:\n")
+        for column in df_showing_nulls:
+            print(df_showing_nulls[column].value_counts(normalize=True))
+        
+        return df_showing_nulls
+
+    pass
---
+++ ExploratoryDataAnalysis/test.py
@@ -0,0 +1,40 @@
+from db_utils import RDSDatabaseConnector as rdbc
+from data_info import DataFrameInfo as dfi
+from data_handling import DataTransformation as dt
+
+# df = rdbc().load_data_from_csv(r"C:\Users\nickc\Desktop\Code\AiCore\ML_Project\loan_payments.csv")
+
+# print(df.columns[0])
+# dfi().column_null_count(df)
+# print(df.info())
+# print(dt().drop_colums_with_significant_null_count(df, 35).info())
+# dt().impute_columns_with_missing_data(dt().correct_field_data_types(dt().drop_colums_with_significant_null_count(df, 35)))
+
+# a = "36 months"
+
+# print(a[:2])
+
+# a = 7.99
+# a = a.lower()
+# print(a)
+# print(type(a))
+
+# meal_to_be_removed = input("What meal do you want to remove?\n")
+# with open("./meal_list.txt", "r") as file:
+#     loaded_meal_list = file.read()
+#     if meal_to_be_removed not in loaded_meal_list:
+#         print("Sorry this meal is not in the meal list.")
+#     else:
+#         with open("./meal_list.txt", "w+") as files:
+#             lines = files.readlines()
+#             print(lines)
+#             for line in lines:
+#                 print(line)
+
+# o = {"a": 1, "b": 2, "c": 3}
+# print(o.items())
+
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(a[-1])
+
+print(round(1.3463, 2))
